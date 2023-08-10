3c593ccb9048b04292c665d9165edb84a5ca9605
Thomas Arnold <ybn4aq@virginia.edu>
2023-08-10 15:40:29 -0400

    Organizing files, starting boiler plate for tiles

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile
+{
+    private bool isMined;
+}
+
+public class Mine : Tile
+{
+
+}
+
+public class NonMine : Tile
+{
+
+}
