d43a399ceccacd5c32569c939f62fe7cdd493570
Thomas Arnold <ybn4aq@virginia.edu>
2023-08-10 15:51:06 -0400

    Updating Tile.cs

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Tile
 {
-    private bool isMined;
+    private bool isMined { get; set; }
+
+    public virtual void Dig()
+    {
+
+    }
+
+    public Tile()
+    {
+        isMined = false;
+    }
 }
 
 public class Mine : Tile
 {
-
+    public override void Dig()
+    {
+        base.Dig();
+        // TODO: call method to end game, possibly emit signal
+    }
 }
 
-public class NonMine : Tile
+public class Blank : Tile
 {
+    private int adjMines { get; set; }
 
+    public override void Dig()
+    {
+        base.Dig();
+    }
+
+
+    public Blank()
+    {
+        adjMines = 0;
+    }
 }
