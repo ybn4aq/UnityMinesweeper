37b5ae0c0a181d5a5975adb45ad2b01aa0baa97f
Thomas Arnold <ybn4aq@virginia.edu>
2023-09-06 09:38:41 -0400

    Added executable
    TODO: add quit button

--- Assets/Scripts/MineCounterScript.cs
+++ Assets/Scripts/MineCounterScript.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System;
 using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class MineCounterScript : MonoBehaviour
